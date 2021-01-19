ed6ce13092368818e4c8fc6f3e24edd7843481fc
InnerPulse <stoev95pd@gmail.com>
2021-01-19 15:18:35 +0200

    Solutions added

--- 01 Syntax, Functions and Statments/08 wordsUppercase.js
+++ 01 Syntax, Functions and Statments/08 wordsUppercase.js
@@ -1,4 +1,6 @@
 function wordsUpercase(str) {
-
-  return str.match(/[\w]+/g).map(x => x.toUpperCase()).join(', ')
+  return str
+    .match(/[\w]+/g)
+    .map((x) => x.toUpperCase())
+    .join(", ");
 }
