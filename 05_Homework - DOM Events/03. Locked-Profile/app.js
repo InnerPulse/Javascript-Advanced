2e3349c242873f55dd934ee20c8e01fed4546aed
InnerPulse <stoev95pd@gmail.com>
2021-01-28 00:16:11 +0200

    Solutions added

---
+++ 05_Homework - DOM Events/03. Locked-Profile/app.js
@@ -0,0 +1,15 @@
+function lockedProfile() {
+   const main = document.querySelectorAll("main div");
+   document.querySelectorAll("button").forEach((b) => b.addEventListener("click", showMore));
+
+   function showMore(ev) {
+      const hidden = ev.target.parentNode.children[9];
+      const unlocked = ev.target.parentNode.children[4].checked;
+
+      if (unlocked) {
+         const isHidden = ev.target.textContent === "Show more";
+         hidden.style.display = isHidden ? "block" : "none";
+         ev.target.textContent = isHidden ? "Hide it" : "Show more";
+      }
+   }
+}
