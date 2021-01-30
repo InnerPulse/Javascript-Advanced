263619f36995bf99ac1a0cf3e4ed120a65d4aacc
InnerPulse <stoev95pd@gmail.com>
2021-01-30 17:20:25 +0200

    Solutions added

---
+++ 04_Homework - DOM Introduction/09. Number Convertor/solution.js
@@ -0,0 +1,27 @@
+function solve() {
+    const output = document.getElementById("result");
+    const binaryOption = document.createElement("option");
+    const hexadecimalOption = document.createElement("option");
+    const selectMenu = document.getElementById("selectMenuTo");
+    binaryOption.textContent = "Binary";
+    binaryOption.setAttribute("value", "binary");
+    hexadecimalOption.textContent = "Hexadecimal";
+    hexadecimalOption.setAttribute("value", "hexadecimal");
+    selectMenu.appendChild(binaryOption);
+    selectMenu.appendChild(hexadecimalOption);
+
+    document.querySelector("button").addEventListener("click", () => {
+        const value = Number(document.getElementById("input").value);
+
+        const convert = {
+            binary: (x) => {
+                output.value = Number(Number(x)).toString(2);
+            },
+            hexadecimal: (x) => {
+                output.value = Number(Number(x)).toString(16).toUpperCase();
+            },
+        };
+        const convertTo = convert[selectMenu.value];
+        convertTo(value);
+    });
+}
