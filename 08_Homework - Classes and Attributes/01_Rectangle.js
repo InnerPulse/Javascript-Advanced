f60a35caaeb9d8f7fe762675cb333c6d5fe60aa4
InnerPulse <stoev95pd@gmail.com>
2021-02-06 18:19:54 +0200

    Solutions added

---
+++ 08_Homework - Classes and Attributes/01_Rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle {
+  constructor(width, height, color) {
+    (this.width = width),
+      (this.height = height),
+      (this.color = color.slice(0, 1).toUpperCase() + color.slice(1));
+  }
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+
+const rect = new Rectangle(4, 5, 'red');
