967602e6a63705e97c25d74c383c0ebfae8670fd
InnerPulse <stoev95pd@gmail.com>
2021-02-10 13:15:08 +0200

    Solutions added

---
+++ 10_Homework - Prototypes and Inheritance/04_Balloons.js
@@ -0,0 +1,44 @@
+function balloons() {
+  class Balloon {
+    constructor(color, gasWeight) {
+      this.color = color;
+      this.gasWeight = gasWeight;
+    }
+  }
+
+  class PartyBalloon extends Balloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength) {
+      super(color, gasWeight);
+      this.ribbon = { color: ribbonColor, length: ribbonLength };
+    }
+
+    set ribbon(value) {
+      this._ribbon = value;
+    }
+
+    get ribbon() {
+      return this._ribbon;
+    }
+  }
+
+  class BirthdayBalloon extends PartyBalloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+      super(color, gasWeight, ribbonColor, ribbonLength);
+      this.text = text;
+    }
+
+    set text(value) {
+      this._text = value;
+    }
+    
+    get text() {
+      return this._text;
+    }
+  }
+
+  return {
+    Balloon: Balloon,
+    PartyBalloon: PartyBalloon,
+    BirthdayBalloon: BirthdayBalloon,
+  };
+}
